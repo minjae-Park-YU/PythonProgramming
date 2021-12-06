3c9fdba6e21802167150440f5f29a6a3406422b3
minjae-Park-YU <59348883+minjae-Park-YU@users.noreply.github.com>
2021-12-06 19:23:49 +0900

    Update pyqt_GUI2.py

--- GUI만들기/pyqt_GUI2.py
+++ GUI만들기/pyqt_GUI2.py
@@ -18,7 +18,7 @@ class Exam(QWidget):
         self.setWindowTitle("Sec Example")
         self.show()
 
-    def closeEvent(self, QCloseEvent):
+    def closeEvent(self, QCloseEvent): # 기본 내장함수에서 Override 한거임
          ans = QMessageBox.question(self, "종료 확인", "종료하시겠습니까?",
                                      QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes) #메시지박스 창 두개
          if ans == QMessageBox.Yes: #Yes 누르면 꺼지고, No 누르면 안꺼짐
